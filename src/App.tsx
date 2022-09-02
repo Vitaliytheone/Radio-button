04fbca8108cbe2ad994848b14e44bea3f64c7728
Vitaliy <Vitaliythesecond@gmail.com>
2022-09-02 23:16:06 +0300

    pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Radio from "./component/radio";
+import styled from "styled-components";
+
+const Label = styled.div`
+    font-size: 24px;
+    font-weight: 500;
+    margin: 16px 0 8px 0;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Label>Radio left variant</Label>
+            <Radio />
+            <Label>Radio right variant</Label>
+            <Radio />
+        </>
+    );
 }
 
 export default App;
